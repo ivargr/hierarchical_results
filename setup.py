f55dbd297c5720201b8cb2216a1a114665261cb1
Ivar Grytten <ivar.grytten@gmail.com>
2023-02-06 13:11:42 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = ['typer', ]
 
 test_requirements = ['pytest>=3', "hypothesis"]
@@ -36,7 +30,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description="",
     include_package_data=True,
     keywords='hierarchical_results',
     name='hierarchical_results',
