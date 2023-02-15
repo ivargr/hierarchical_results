ad08253a8dab37d4686e322eeeb9a9ecd59e940e
BumpVersion Action <bumpversion@github-actions>
2023-02-15 15:49:00 +0000

    Bump version: 0.0.1 → 0.0.2

--- hierarchical_results/__init__.py
+++ hierarchical_results/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ivar Grytten"""
 __email__ = 'ivargry@ifi.uio.no'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 
 from .hierarchical_results import HierarchicalResults, ParameterCombinations
